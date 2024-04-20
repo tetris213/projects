55f3829ff0800126c4aa9894cbf2b47302ca12aa
Tozzi <tozzi.man@gmail.com>
2024-04-20 10:50:36 -0400

    first PR of new repo

---
+++ algo_demos/python/main.py
@@ -0,0 +1,84 @@
+import pygame
+
+
+def main():
+    pygame.init()
+    DISPLAYSURF = pygame.display.set_mode((1280, 720))
+    FPS = pygame.time.Clock()
+    pygame.display.set_caption("Algo Demo")
+    running = True
+
+    DISPLAYSURF.fill("black")
+
+    list = LinkedList(5)
+    list.draw(DISPLAYSURF)
+    while running:
+        for event in pygame.event.get():
+            #   start handling clicking on buttons
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.flip()
+        FPS.tick(60)
+    pygame.quit()
+
+
+class LinkedList(pygame.sprite.Sprite):
+    array = []
+    color = "white"
+    window_info = None
+    lr_position = -175
+    heigh_position = None
+    push_btn = None
+    pop_btn = None
+    clear_btn = None
+    input_field = None
+    font = None
+
+    # handle input for list
+    def __init__(self, number_of_cells):
+        super().__init__()
+        self.window_info = pygame.display.get_window_size()
+        self.height_position = 4 * self.window_info[1] / 5
+        self.font = pygame.font.Font("freesansbold.ttf", 12)
+        self.init_array(number_of_cells)
+        self.init_controls()
+
+    def init_array(self, number_of_cells):
+        x = self.lr_position
+        y = self.height_position
+        for i in range(number_of_cells):
+            x += self.window_info[0] / number_of_cells
+            cell = pygame.Rect((x, y), (100, 100))
+            self.array.append(cell)
+
+    def init_controls(self):
+        self.push_btn = pygame.Rect((1105, 100), (100, 50))
+        self.pop_btn = pygame.Rect((905, 100), (100, 50))
+        self.clear_btn = pygame.Rect((705, 100), (100, 50))
+        self.input_field = pygame.Rect((505, 100), (100, 50))
+
+    def drawText(self, rect, DISPLAYSURF, text):
+        text_object = self.font.render(text, True, (0, 0, 0), (255, 255, 255))
+        centered_text = text_object.get_rect(center=rect.center)
+        pygame.Surface.blit(DISPLAYSURF, text_object, centered_text)
+
+    def handleInput(self):
+        pass
+
+    def draw(self, DISPLAYSURF):
+        for cell in self.array:
+            pygame.draw.rect(DISPLAYSURF, self.color, cell)
+        pygame.draw.rect(DISPLAYSURF, self.color, self.push_btn)
+        pygame.draw.rect(DISPLAYSURF, self.color, self.pop_btn)
+        pygame.draw.rect(DISPLAYSURF, self.color, self.clear_btn)
+        pygame.draw.rect(DISPLAYSURF, "grey", self.input_field)
+        self.drawText(self.push_btn, DISPLAYSURF, "PUSH")
+        self.drawText(self.input_field, DISPLAYSURF, "INPUT")
+        self.drawText(self.pop_btn, DISPLAYSURF, "POP")
+        self.drawText(self.clear_btn, DISPLAYSURF, "CLEAR")
+
+
+if __name__ == "__main__":
+    main()
---
+++ sf_clone/README.md
@@ -0,0 +1,29 @@
+Ideas:
+
+not story heavy mostly through environment?
+metoidvania
+combat
+stranded on planet for whatever reason with damaged ship
+must find a way off the planet by upgrading ship
+culminates with a cool space battle
+
+starfox/bodyharvest type game:
+1 open world
+would be cool to have it be a giant planet that you can move fully around
+underwater areas?
+smaller dungeons?
+space station
+giant ship
+asteroid?
+galactic anomaly
+
+vehicles are main way of overcoming obstacles
+pilot 
+tank
+hover tank (cannot fly, over water? lava?)
+submarine (under water)
+jet
+super machine (does it all sortve like samus with the metroid)
+
+
+
